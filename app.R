8527cd92bfec94659e71eee94ae7287fd1be5433
Anthony Sena <anthonysena@users.noreply.github.com>
2024-07-24 19:25:12 -0400

    Project init (#1)

---
+++ app.R
@@ -0,0 +1,33 @@
+library(OhdsiShinyModules)
+library(ShinyAppBuilder)
+
+shinyConfig <- initializeModuleConfig() |>
+  addModuleConfig(
+    createDefaultAboutConfig()
+  )  |>
+  addModuleConfig(
+    createDefaultDatasourcesConfig()
+  )  |>
+  addModuleConfig(
+    createDefaultCohortGeneratorConfig()
+  ) |>
+  addModuleConfig(
+    createDefaultCharacterizationConfig()
+  )
+
+cli::cli_h1("Starting shiny server")
+serverStr <- paste0(Sys.getenv("shinydbServer"), "/", Sys.getenv("shinydbDatabase"))
+cli::cli_alert_info("Connecting to {server}")
+connectionDetails <- DatabaseConnector::createConnectionDetails(
+  dbms = "postgresql",
+  server = serverStr, #Sys.getenv("shinydbServer"),
+  port = Sys.getenv("shinydbPort"),
+  user = "shinyproxy",
+  password = Sys.getenv("shinydbPw")
+)
+
+cli::cli_h2("Loading schema")
+createShinyApp(config = shinyConfig,
+               connectionDetails = connectionDetails,
+               resultDatabaseSettings = createDefaultResultDatabaseSettings(schema = "semanaion")
+)
